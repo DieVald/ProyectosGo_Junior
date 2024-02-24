3ac6fa3557b62d45c50df78d21da90aefe7312ba
Diego Valderrama <diego.valderama.personal@gmail.com>
2024-02-24 00:06:17 -0500

    probando constantes, variables y funciones 01

---
+++ Proyectos_Go_YoutubeDomini/Hola-Mundo/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hola señor Platzi: \n")
+
+	//*** Declaracion de Constantes:
+	const pi float64 = 3.14
+	const pi2 float64 = 3.1416
+
+	fmt.Println("PI es:  ", pi, "\npi2 es: ", pi2)
+
+	//*** Declaración de Variables Enteras
+	base := 12
+	var altura int = 14
+	var area int
+}
